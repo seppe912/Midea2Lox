d463cecfec8d2a8aa2d1810f7d8a39c04aae3818
seppe912 <39451979+seppe912@users.noreply.github.com>
2019-03-09 09:12:20 +0100

    Update postupgrade.sh

--- postupgrade.sh
+++ postupgrade.sh
@@ -13,5 +13,24 @@ cp -p -v -r /tmp/$ARGV1\_upgrade/config/$ARGV3/* $ARGV5/config/plugins/$ARGV3/
 echo "<INFO> Remove temporary folders"
 rm -r /tmp/$ARGV1\_upgrade
 
+echo "<INFO> start Midea2Lox"
+is_running() {
+	/bin/ps -C "midea2lox.py" -opid= > /dev/null 2>&1
+}
+
+if is_running; then
+    PID=`/bin/ps -C "midea2lox.py" -opid=`
+    killall midea2lox.py
+else
+    echo "Starting Midea2Lox..."
+if [ $EUID -eq 0 ]; then
+    cd $ARGV1/data/plugins/$ARGV3
+    su loxberry -c ./midea2lox.py > /dev/null 2>&1 &
+else
+    cd $ARGV1/data/plugins/$ARGV3
+    ./midea2lox.py > /dev/null 2>&1 &
+fi
+fi
+
 # Exit with Status 0
 exit 0
