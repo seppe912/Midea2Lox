3977c46681beed1f98d48b0c9a293ea81c3133e4
seppe912 <39451979+seppe912@users.noreply.github.com>
2020-05-30 08:20:39 +0200

    clean code

--- data/getID.py
+++ data/getID.py
@@ -1,30 +1,22 @@
 #!/usr/bin/python3
 # -*- coding: utf-8 -*-
 
-# Update Midea status
-#Start
-
-
-# Update Midea status
-
 from midea.client import client as midea_client
-from midea.device import air_conditioning_device as ac
 
-import sys
-import requests
 import configparser
 import logging
 
-# Miniserver Daten Laden
+# Midea Cloud Zugangsdaten laden
 cfg = configparser.ConfigParser()
 cfg.read('REPLACELBPCONFIGDIR/midea2lox.cfg')
 MideaUser = cfg.get('default','MideaUser')
 MideaPassword = cfg.get('default','MideaPassword')
 
+# logging
 _LOGGER = logging.getLogger(__name__)
 logging.basicConfig(level=logging.INFO, filename='REPLACELBPLOGDIR/midea2lox.log', format='%(asctime)s %(name)-12s %(levelname)-8s %(message)s', datefmt='%d.%m %H:%M')
 
-#Start
+# Start getID
 _LOGGER.info("connecting to Midea Cloud") 
 # The client takes an App_Key, an account email address and a password
 client = midea_client('3742e9e5842d4ad59c2db887e12449f9', MideaUser, MideaPassword)
@@ -34,13 +26,10 @@ client.setup()
 
 # List devices. These are complex state holding objects, no other trickery needed from here. 
 devices = client.devices()
-  
 count = 0
 
 for device in devices:
 
     count +=1
-    # Refresh the object with the actual state by querying it
-    device.refresh()    
     print("Appliance %s, Name: %s, ID: %s" % (count, device.name, device.id))
     _LOGGER.info("Appliance {}: Name: {}, ID: {}".format(count, device.name, device.id))
