7de5a79a960707a51796e4d530055fa4f78e4cdb
seppe912 <39451979+seppe912@users.noreply.github.com>
2020-06-11 11:08:34 +0200

    Update postinstall.sh

--- postinstall.sh
+++ postinstall.sh
@@ -49,7 +49,7 @@ ARGV3=$3 # Third argument is Plugin installation folder
 ARGV5=$5 # Fifth argument is Base folder of LoxBerry
 
 chmod +x $PDATA/midea2lox.py
-chmod +x $PDATA/getID.py
+chmod +x $PDATA/discover.py
 
 
 /bin/echo "#############################################################################################"
