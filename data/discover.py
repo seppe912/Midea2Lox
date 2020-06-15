0741b5cbc86443d4a0e9552fe4dafd86ff63bf4e
seppe912 <39451979+seppe912@users.noreply.github.com>
2020-06-15 19:44:14 +0200

    fix debug logging on discover

--- data/discover.py
+++ data/discover.py
@@ -2,28 +2,27 @@
 # -*- coding: utf-8 -*-
 import logging
 import sys
-
 import configparser
 
 cfg = configparser.RawConfigParser()
 cfg.read('REPLACELBPCONFIGDIR/midea2lox.cfg')
 DEBUG = cfg.get('default','DEBUG')
 
-try:
+    try:
     from msmart.cli import discover
 
-    # logging
-    _LOGGER = logging.getLogger(__name__)
-    logging.basicConfig(level=logging.INFO, filename='REPLACELBPLOGDIR/midea2lox.log', format='%(asctime)s %(name)-12s %(levelname)-8s %(message)s', datefmt='%d.%m %H:%M')
-
+    if DEBUG == "1":
+        _LOGGER = logging.getLogger(__name__)
+        logging.basicConfig(level=logging.DEBUG, filename='REPLACELBPLOGDIR/midea2lox.log', format='%(asctime)s %(name)-12s %(levelname)-8s %(message)s', datefmt='%d.%m %H:%M')
+        discover(1)
+    else:
+        _LOGGER = logging.getLogger(__name__)
+        logging.basicConfig(level=logging.INFO, filename='REPLACELBPLOGDIR/midea2lox.log', format='%(asctime)s %(name)-12s %(levelname)-8s %(message)s', datefmt='%d.%m %H:%M')    
+        discover(0)
 except:
     _LOGGER = logging.getLogger(__name__)
     logging.basicConfig(level=logging.INFO, filename='REPLACELBPLOGDIR/midea2lox.log', format='%(asctime)s %(name)-12s %(levelname)-8s %(message)s', datefmt='%d.%m %H:%M')
     print('Error : ' + str(sys.exc_info()))
     _LOGGER.error(str(sys.exc_info()))
     sys.exit()
-
-if DEBUG == "1":
-    discover(1)
-else:
-    discover(0)
+ 
