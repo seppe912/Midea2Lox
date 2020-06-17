8931cb3428c2d1899ffa19a679a425caae8e97c4
seppe912 <39451979+seppe912@users.noreply.github.com>
2020-06-17 21:25:47 +0200

    Update discover.py

--- data/discover.py
+++ data/discover.py
@@ -12,15 +12,15 @@ try:
     from msmart.cli import discover
 
     if DEBUG == "1":
-        _LOGGER = logging.getLogger(__name__)
+        _LOGGER = logging.getLogger("discover.py")
         logging.basicConfig(level=logging.DEBUG, filename='REPLACELBPLOGDIR/midea2lox.log', format='%(asctime)s %(name)-12s %(levelname)-8s %(message)s', datefmt='%d.%m %H:%M')
         discover(1)
     else:
-        _LOGGER = logging.getLogger(__name__)
+        _LOGGER = logging.getLogger("discover.py")
         logging.basicConfig(level=logging.INFO, filename='REPLACELBPLOGDIR/midea2lox.log', format='%(asctime)s %(name)-12s %(levelname)-8s %(message)s', datefmt='%d.%m %H:%M')    
         discover(0)
 except:
-    _LOGGER = logging.getLogger(__name__)
+    _LOGGER = logging.getLogger("discover.py")
     logging.basicConfig(level=logging.INFO, filename='REPLACELBPLOGDIR/midea2lox.log', format='%(asctime)s %(name)-12s %(levelname)-8s %(message)s', datefmt='%d.%m %H:%M')
     print('Error : ' + str(sys.exc_info()))
     _LOGGER.error(str(sys.exc_info()))
