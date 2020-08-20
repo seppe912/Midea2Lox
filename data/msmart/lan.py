16400ff71c5a806f7df2ab52bf43332bb59d5805
seppe912 <39451979+seppe912@users.noreply.github.com>
2020-08-20 18:05:38 +0200

    Update lan.py

--- data/msmart/lan.py
+++ data/msmart/lan.py
@@ -4,12 +4,11 @@ import datetime
 import socket
 import sys
 import time
-from msmart.security import security
+from msmart.security import security, MSGTYPE_HANDSHAKE_REQUEST, MSGTYPE_ENCRYPTED_REQUEST
 
 VERSION = '0.1.20'
 
 _LOGGER = logging.getLogger(__name__)
-
 
 class lan:
     def __init__(self, device_ip, device_id):
@@ -18,23 +17,41 @@ class lan:
         self.device_port = 6444
         self.security = security()
         self._retries = 0
+        self._socket = None
+        self._token = None
+        self._key = None
+
+    def _connect(self):
+        if self._socket == None:
+            _LOGGER.debug("Attempting new connection to {}:{}".format(
+                self.device_ip, self.device_port))
+            self._buffer = b''
+            self._socket = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+            self._socket.settimeout(8)
+            try:
+                self._socket.connect((self.device_ip, self.device_port))
+            except:
+                self._disconnect()
+
+    def _disconnect(self):
+        if self._socket:
+            self._socket.close()
+            self._socket = None
 
     def request(self, message, broadcast):
         # Create a TCP/IP socket
-        sock = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
-        sock.settimeout(8)
+        self._connect()
         
         try:
-            # Connect the Device
-            device_address = (self.device_ip, self.device_port)
-            sock.connect(device_address)
+            if self._socket == None:
+                raise socket.error
             # Send data
             _LOGGER.debug("Sending to {}:{} {}".format(
                 self.device_ip, self.device_port, message.hex()))
-            sock.sendall(message)
+            self._socket.send(message)
 
             # Received data
-            response = sock.recv(512)
+            response = sock.recv(1024)
         except socket.timeout:
             _LOGGER.info("Connect the Device %s:%s TimeOut for 8s. don't care about a small amount of this. if many maybe not support".format(
                 self.device_ip, self.device_port))
@@ -51,6 +68,7 @@ class lan:
                     _LOGGER.info("retry %s/20 @ %s:%s " %(self._retries, self.device_ip, self.device_port))
                     return self.request(message, broadcast)
                 else:
+                    self._disconnect()
                     return bytearray(0)
         except socket.error:
             if broadcast:
@@ -66,37 +84,42 @@ class lan:
                     _LOGGER.info("retry %s/40 @ %s:%s " %(self._retries, self.device_ip, self.device_port))
                     return self.request(message, broadcast)
                 else:
+                    self._disconnect()
                     sys.exit("Socket Error! Please Check your IP and ID from the AC and that your AC is connected to your Router")
-        finally:
-            sock.close()
         _LOGGER.debug("Received from {}:{} {}".format(
-            self.device_ip, self.device_port, message.hex()))
+            self.device_ip, self.device_port, response.hex()))
         return response
 
-    def encode(self, data: bytearray):
-        normalized = []
-        for b in data:
-            if b >= 128:
-                b = b - 256
-            normalized.append(str(b))
+    def authenticate(self, mac: str, ssid: str, pw: str):
+        self._token, self._key = self.security.token_key_pair(mac, ssid, pw)
+        self._authenticate()
+        
+    def _authenticate(self):
+        if not self._token or not self._key:
+            raise Exception('missing token key pair')
+        request = self.security.encode_8370(self._token, MSGTYPE_HANDSHAKE_REQUEST)
+        response = self.request(request)[8:72]
+        try:
+            tcp_key = self.security.tcp_key(response, self._key)
+            _LOGGER.debug('Got TCP key for {}:{} {}'.format(self.device_ip, self.device_port, tcp_key.hex()))
+        except Exception as error:
+            self._disconnect()
+            raise error
 
-        string = ','.join(normalized)
-        return bytearray(string.encode('ascii'))
-
-    def decode(self, data: bytearray):
-        data = [int(a) for a in data]
-        for i in range(len(data)):
-            if data[i] < 0:
-                data[i] = data[i] + 256
-        return bytearray(data)
-
+    def appliance_transparent_send_8370(self, data, msgtype=MSGTYPE_ENCRYPTED_REQUEST):
+        if self._socket == None:
+            self._authenticate()
+        data = self.security.encode_8370(data, msgtype)
+        responses, self._buffer = self.security.decode_8370(self._buffer + self.request(data))
+        packets = []
+        for response in responses:
+            if len(response) > 40 + 16:
+                response = self.security.aes_decrypt(response[40:-16])
+            packets.append(response)
+        return packets
+            
     def appliance_transparent_send(self, data, broadcast):
-        response = bytearray(self.request(data, broadcast))
-        if len(response) > 0:
-            if len(response) == 88:
-                reply = self.decode(self.security.aes_decrypt(response[40:72]))
-            else:
-                reply = self.decode(self.security.aes_decrypt(response[40:88]))
-            return reply
-        else:
-            return bytearray(0)
+        response = self.request(data)
+        if len(response) > 40 + 16:
+            return [self.security.aes_decrypt(response[40:-16])]
+        return [response]
