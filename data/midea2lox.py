f88f7544d22acd6d972504463eea1927fff74444
seppe912 <39451979+seppe912@users.noreply.github.com>
2021-08-24 20:25:15 +0200

    Update midea2lox.py

--- data/midea2lox.py
+++ data/midea2lox.py
@@ -238,17 +238,17 @@ def send_to_loxone(device, support_mode):
     
     address_loxone = ("http://%s:%s@%s:%s/dev/sps/io/" % (LoxUser, LoxPassword, LoxIP, LoxPort))    
     addresses = [
-        ("%sMidea.%s.power_state,%s" % (address_loxone, device.id, int(device.power_state))),                #power_state
-        ("%sMidea.%s.audible_feedback,%s" % (address_loxone, device.id, int(device.prompt_tone))),           #prompt_tone
-        ("%sMidea.%s.target_temperature,%s" % (address_loxone, device.id, device.target_temperature)),  #target_temperature
-        ("%sMidea.%s.operational_mode,%s" % (address_loxone, device.id, device.operational_mode)),      #operational_mode
-        ("%sMidea.%s.fan_speed,%s" % (address_loxone, device.id, device.fan_speed)),                    #fan_speed
-        ("%sMidea.%s.swing_mode,%s" % (address_loxone, device.id, device.swing_mode)),                  #swing_mode
-        ("%sMidea.%s.eco_mode,%s" % (address_loxone, device.id, int(device.eco_mode))),                      #eco_mode
-        ("%sMidea.%s.turbo_mode,%s" % (address_loxone, device.id, int(device.turbo_mode))),                  #turbo_mode
-        ("%sMidea.%s.indoor_temperature,%s" % (address_loxone, device.id, device.indoor_temperature)),  #indoor_temperature
-        ("%sMidea.%s.outdoor_temperature,%s" % (address_loxone, device.id, device.outdoor_temperature)), #outdoor_temperature
-        ("%sMidea.%s.online,%s" % (address_loxone, device.id, int(device.online)))                      #device.online
+        ("%sMidea/%s/power_state,%s" % (address_loxone, device.id, int(device.power_state))),                #power_state
+        ("%sMidea/%s/audible_feedback,%s" % (address_loxone, device.id, int(device.prompt_tone))),           #prompt_tone
+        ("%sMidea/%s/target_temperature,%s" % (address_loxone, device.id, device.target_temperature)),  #target_temperature
+        ("%sMidea/%s/operational_mode,%s" % (address_loxone, device.id, device.operational_mode)),      #operational_mode
+        ("%sMidea/%s/fan_speed,%s" % (address_loxone, device.id, device.fan_speed)),                    #fan_speed
+        ("%sMidea/%s/swing_mode,%s" % (address_loxone, device.id, device.swing_mode)),                  #swing_mode
+        ("%sMidea/%s/eco_mode,%s" % (address_loxone, device.id, int(device.eco_mode))),                      #eco_mode
+        ("%sMidea/%s/turbo_mode,%s" % (address_loxone, device.id, int(device.turbo_mode))),                  #turbo_mode
+        ("%sMidea/%s/indoor_temperature,%s" % (address_loxone, device.id, device.indoor_temperature)),  #indoor_temperature
+        ("%sMidea/%s/outdoor_temperature,%s" % (address_loxone, device.id, device.outdoor_temperature)), #outdoor_temperature
+        ("%sMidea/%s/online,%s" % (address_loxone, device.id, int(device.online)))                      #device.online
         ]
     
     if MQTT == 1 and support_mode == 0: # publish over MQTT
