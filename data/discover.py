ee7fe9a1d8c4c7e538b5a96898e2cf28610d2058
seppe912 <39451979+seppe912@users.noreply.github.com>
2021-09-05 09:50:24 +0200

    Update discover.py

--- data/discover.py
+++ data/discover.py
@@ -10,25 +10,25 @@ log_path = 'REPLACELBPLOGDIR' #### REPLACE LBPLOGDIR ####
 home_path = 'REPLACELBHOMEDIR' #### REPLACE LBHOMEDIR ####
 
 cfg = configparser.RawConfigParser()
-cfg.read(cfg_path + 'midea2lox.cfg')
+cfg.read(cfg_path + '/midea2lox.cfg')
 DEBUG = cfg.get('default','DEBUG')
 MideaUser = cfg.get('default','MideaUser')
 MideaPW = cfg.get('default','MideaPassword')
 BroadcastPakets = cfg.get('default','BroadcastPakets')
 
 cfg_devices = configparser.RawConfigParser()
-cfg_devices.read(cfg_path + 'devices.cfg')
+cfg_devices.read(cfg_path + '/devices.cfg')
 
 try:
     from msmart.cli import discover
 
     if DEBUG == "1":
         _LOGGER = logging.getLogger("discover.py")
-        logging.basicConfig(level=logging.DEBUG, filename= log_path + 'midea2lox.log', format='%(asctime)s %(name)-12s %(levelname)-8s %(message)s', datefmt='%d.%m %H:%M')
+        logging.basicConfig(level=logging.DEBUG, filename= log_path + '/midea2lox.log', format='%(asctime)s %(name)-12s %(levelname)-8s %(message)s', datefmt='%d.%m %H:%M')
         device_list = discover(1,int(BroadcastPakets), MideaUser, MideaPW)
     else:
         _LOGGER = logging.getLogger("discover.py")
-        logging.basicConfig(level=logging.INFO, filename= log_path + 'midea2lox.log', format='%(asctime)s %(name)-12s %(levelname)-8s %(message)s', datefmt='%d.%m %H:%M')    
+        logging.basicConfig(level=logging.INFO, filename= log_path + '/midea2lox.log', format='%(asctime)s %(name)-12s %(levelname)-8s %(message)s', datefmt='%d.%m %H:%M')    
         device_list = discover(0,int(BroadcastPakets), MideaUser, MideaPW)
 
     for eachArg in device_list:
@@ -45,12 +45,12 @@ try:
             cfg_devices.set('Midea_' + str(device['id']),"token", str(device['token']))
             cfg_devices.set('Midea_' + str(device['id']),"key", str(device['key']))
             
-        cfg_devices.write(open(log_path + 'devices.log','w'))
-        cfg_devices.write(open(cfg_path + 'devices.cfg','w'))
+        cfg_devices.write(open(log_path + '/devices.log','w'))
+        cfg_devices.write(open(cfg_path + '/devices.cfg','w'))
         
 except:
     _LOGGER = logging.getLogger("discover.py")
-    logging.basicConfig(level=logging.INFO, filename= log_path + 'midea2lox.log', format='%(asctime)s %(name)-12s %(levelname)-8s %(message)s', datefmt='%d.%m %H:%M')
+    logging.basicConfig(level=logging.INFO, filename= log_path + '/midea2lox.log', format='%(asctime)s %(name)-12s %(levelname)-8s %(message)s', datefmt='%d.%m %H:%M')
     print('Error : ' + str(sys.exc_info()))
     _LOGGER.error(str(sys.exc_info()))
     sys.exit()
